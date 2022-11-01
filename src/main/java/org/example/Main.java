cac4730bf32b600b2dc153f54383d2af60fbef00
ZhouSusan <szhou089@gmail.com>
2022-10-31 23:17:52 -0700

    added getMax() to Main.java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static int getMax(int[] list) {
+        int max = list[0];
+        for (int i = 1; i < list.length; i++) {
+            if (list[i] > max) {
+                max = list[i];
+            }
+        }
+        return max;
+    }
+}
